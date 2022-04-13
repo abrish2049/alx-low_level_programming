12b77dad035337773dfe4d8d51222bdd6e89e1c6
abrish2049 <abrahammenberu@gmail.com>
2022-04-13 11:02:45 -0700

    fun

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - Alpha
+ *
+ * Return: Always
+ */
+void print_alphabet(void)
+{
+	char a;
+
+	for ( c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+
+	_putchar('\n');
+}
