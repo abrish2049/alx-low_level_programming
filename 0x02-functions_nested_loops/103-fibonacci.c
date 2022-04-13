0fc57aa9679e0bd6307a52beaaf22c0d920202ef
abrish2049 <abrahammenberu@gmail.com>
2022-04-13 12:29:18 -0700

    fun

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,29 +1,23 @@
 #include <stdio.h>
 
-/**
- * main - prints fibon
+/** 
+ * main - Prints sum
  *
- * Return: Nothing!
+ * Return: always
  */
 int main(void)
 {
 	int i = 0;
-	long j = 1, k = 2;
-	
-	while (i < 50)
+	long j = 1, k = 2, sum = k;
+
+	while (k + j < 4000000)
 	{
-		if (i == 0)
-			printf("%ld", j);
-		else if (i == 1)
-			printf(", %ld", k);
-		else
-		{
-			k += j;
+		k += j;
+		if (k % 2 == 0)
+			sum += k;
 			j = k - j;
-			printf(", %ld", k);
-		}
-		++i;
+			++i;
 	}
-	printf("\n");
+	printf("%ld\n", sum);
 	return (0);
 }
