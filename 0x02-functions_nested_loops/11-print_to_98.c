1b8646d7b4a75ea928fe381449fd0f4d6de21587
abrish2049 <abrahammenberu@gmail.com>
2022-04-13 12:00:16 -0700

    fun

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,44 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_to_98 - Prints all nos till 98
+ * @n: the nos 
+ *
+ * Return: Always 0
+ */
+void print_to_98(int n)
+{
+	if (n <= 98)
+	{
+		for (; n <= 98; n++)
+		{
+			if (n == 98)
+			{
+				printf("%d", n);
+				printf("\n");
+				break;
+			}
+			else
+			{
+				printf("%d, ", n);
+			}
+		}
+	}
+	else
+	{
+		for (; n >= 98; n--)
+		{
+			if (n == 98)
+			{
+				printf("%d", n);
+				printf("\n");
+				break;
+			}
+			else
+			{
+				printf("%d, ", n);
+			}
+		}
+	}
+}
