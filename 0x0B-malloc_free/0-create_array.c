94541bff8dec61dc324cbfe2a40d3ff2741e534e
abrish2049 <abrahammenberu@gmail.com>
2022-05-03 02:38:06 -0700

    alloc

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -2,35 +2,33 @@
 #include <stdlib.h>
 
 /**
-  * _strdup - Duplicate a string
-  * @str: the string to duplicate
-  *
-  * Return: the string duplicated
-  */
+ * _strdup - Returns a pointer to a newly-allocated space in memory
+ *           containing a copy of the string given as parameter.
+ * @str: The string to be copied.
+ *
+ * Return: If str == NULL or insufficient memory is available - NULL.
+ *         Otherwise - a pointer to the duplicated string.
+ */
 char *_strdup(char *str)
 {
-	int a = 0, i = 1;
-	char *s;
+	char *duplicate;
+	int index, len = 0;
 
 	if (str == NULL)
 		return (NULL);
 
-	while (str[i])
-	{
-		i++;
-	}
+	for (index = 0; str[index]; index++)
+		len++;
 
-	s = malloc((sizeof(char) * i) + 1);
+	duplicate = malloc(sizeof(char) * (len + 1));
 
-	if (s == NULL)
+	if (duplicate == NULL)
 		return (NULL);
 
-	while (a < i)
-	{
-		s[a] = str[a];
-		a++;
-	}
+	for (index = 0; str[index]; index++)
+		duplicate[index] = str[index];
 
-	s[a] = '\0';
-	return (s);
+	duplicate[len] = '\0';
+
+	return (duplicate);
 }
